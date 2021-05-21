4bd790a98dcaa40c14b45ec46747eeb3d56c157a
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2021-05-21 01:19:43 -0300

    remove qualificador desnecessario

--- src/DTO/ConsultaVenda.php
+++ src/DTO/ConsultaVenda.php
@@ -6,10 +6,10 @@ namespace Fastpay\Client\DTO;
 
 use Fastpay\Client\Interfaces\FactoryMethod;
 
-class ConsultaVenda implements \Fastpay\Client\Interfaces\FactoryMethod
+class ConsultaVenda implements FactoryMethod
 {
 
-    public static function construct(array $data): \Fastpay\Client\Interfaces\FactoryMethod
+    public static function construct(array $data): FactoryMethod
     {
         // TODO: Implement construct() method.
     }
