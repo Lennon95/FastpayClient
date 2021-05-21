0bb3b453d4a68ceac1505049863126ef60c381f7
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2021-05-21 01:24:53 -0300

    esqueci de add strict types em todos os dtos

--- src/DTO/Venda.php
+++ src/DTO/Venda.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 
 namespace Fastpay\Client\DTO;
@@ -8,9 +8,31 @@ use Fastpay\Client\Interfaces\FactoryMethod;
 
 class Venda implements FactoryMethod
 {
+    private string $numero;
+    private Cliente $cliente;
+    private \DateTimeImmutable $dataVenda;
+    private string $nroReferencia;
+    private string $tipo;
+    private string $tipoVenda;
+    private string $situacao;
+    private int $nroParcelas;
+    private float $valor;
+    private array $parcelas;
 
     public static function construct(array $data): Venda
     {
-        // TODO: Implement construct() method.
+        /*
+        "nu_venda": "34915-Alv7z-SuBCK",
+      "nm_cliente": "CONTA SERASA TESTE CPF",
+      "nu_documento": "05384515805",
+      "dt_venda": "2021-04-04",
+      "nu_referencia": "1617545384",
+      "tipo": "CREDITO",
+      "tipo_venda": "PB",
+      "situacao": "ATIVO",
+      "nu_parcelas": 10,
+      "vl_venda": "1300.00",
+      "parcelas"
+        */
     }
 }
